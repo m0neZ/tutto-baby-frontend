7cba091490d9236508e55ca1285e74df159ee2e0
m0neZ <ramonkattan@gmail.com>
2025-04-08 20:35:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductList from './ProductList';
+import ProductForm from './ProductForm';
 
 function App() {
+  const [refreshFlag, setRefreshFlag] = useState(false);
+
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>Pima Inventory Frontend</h1>
-      <ProductList />
+      <ProductForm onProductAdded={() => setRefreshFlag(!refreshFlag)} />
+      <ProductList refreshFlag={refreshFlag} />
     </div>
   );
 }
