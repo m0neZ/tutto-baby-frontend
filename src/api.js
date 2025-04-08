6adefed3f544ebf1c5361c270408f1f2c82713f5
m0neZ <ramonkattan@gmail.com>
2025-04-08 15:31:28 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:5000/api';
+const BASE_URL = 'https://your-backend-name.onrender.com/api';
 
 export const fetchProducts = async () => {
   const res = await fetch(`${BASE_URL}/products/`);
