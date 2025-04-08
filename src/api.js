1fa8b606899100978abdaef7eb322079894780aa
m0neZ <ramonkattan@gmail.com>
2025-04-08 16:43:59 -0300

    Point API to live backend

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://tutto-baby-backend.onrender.com';
+const BASE_URL = 'https://tutto-baby-backend.onrender.com/api';
 
 export const fetchProducts = async () => {
   const res = await fetch(`${BASE_URL}/products/`);
