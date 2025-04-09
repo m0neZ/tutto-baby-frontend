5a4092d121404a96d46eaf0a171a0a5a351a82f4
m0neZ <ramonkattan@gmail.com>
2025-04-08 22:24:16 -0300

    Create AdminPage.jsx

---
+++ src/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPage = () => {
+  return (
+    <div style={{ paddingTop: '100px', padding: '2rem' }}>
+      <h2>Gerenciar Campos</h2>
+      <p>Aqui você poderá gerenciar as opções de Tamanho, Cor/Estampa e outros campos dinâmicos.</p>
+    </div>
+  );
+};
+
+export default AdminPage;
