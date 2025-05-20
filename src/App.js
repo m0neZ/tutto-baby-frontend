949ff368b60d8b872665f0b97171b6501ec72cd9
m0neZ <ramonkattan@gmail.com>
2025-05-20 19:07:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-// src/App.js
+// File: src/App.js
 import React, { useContext } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from './auth/AuthContext';
+import { Box } from '@mui/material';
+import Sidebar from './components/Sidebar';
 import EstoquePage from './pages/EstoquePage';
 import VendasPage from './pages/VendasPage';
 import ClientesPage from './pages/ClientesPage';
@@ -17,49 +19,55 @@ function PrivateRoute({ children }) {
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <EstoquePage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/vendas"
-          element={
-            <PrivateRoute>
-              <VendasPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/clientes"
-          element={
-            <PrivateRoute>
-              <ClientesPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/relatorios"
-          element={
-            <PrivateRoute>
-              <RelatoriosPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <PrivateRoute>
-              <AdminPage />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+        <Sidebar />
+        <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<Navigate to="/estoque" replace />} />
+            <Route
+              path="/estoque"
+              element={
+                <PrivateRoute>
+                  <EstoquePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/vendas"
+              element={
+                <PrivateRoute>
+                  <VendasPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/clientes"
+              element={
+                <PrivateRoute>
+                  <ClientesPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/relatorios"
+              element={
+                <PrivateRoute>
+                  <RelatoriosPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/admin"
+              element={
+                <PrivateRoute>
+                  <AdminPage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Box>
+      </Box>
     </BrowserRouter>
   );
 }
