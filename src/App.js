78ba17b702458fc21139847281c85c5b1d9b8739
m0neZ <ramonkattan@gmail.com>
2025-05-20 21:34:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-// File: src/App.js
+import React, { useContext } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthContext } from './auth/AuthContext';
+import Layout from './components/Layout';
+import EstoquePage from './pages/EstoquePage';
+import VendasPage from './pages/VendasPage';
+import ClientesPage from './pages/ClientesPage';
+import RelatoriosPage from './pages/RelatoriosPage';
+import AdminPage from './AdminPage';
+import LoginPage from './pages/LoginPage';
 
-import React, { useContext } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  Outlet,
-  useLocation,
-} from "react-router-dom";
-import { AuthContext } from "./auth/AuthContext";
-import { Box } from "@mui/material";
-
-import Sidebar from "./components/Sidebar";
-import EstoquePage from "./pages/EstoquePage";
-import VendasPage from "./pages/VendasPage";
-import ClientesPage from "./pages/ClientesPage";
-import RelatoriosPage from "./pages/RelatoriosPage";
-import AdminPage from "./AdminPage";
-import LoginPage from "./pages/LoginPage";
-
-function PrivateLayout() {
+function PrivateRoute({ children }) {
   const { token } = useContext(AuthContext);
-  const location = useLocation();
-
-  if (!token) {
-    return <Navigate to="/login" replace state={{ from: location }} />;
-  }
-
-  return (
-    <Box sx={{ display: "flex", minHeight: "100vh" }}>
-      <Sidebar />
-      <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
-        <Outlet />
-      </Box>
-    </Box>
-  );
+  return token ? children : <Navigate to="/login" replace />;
 }
 
 export default function App() {
@@ -43,17 +19,57 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-
-        <Route element={<PrivateLayout />}>
-          <Route path="/estoque" element={<EstoquePage />} />
-          <Route path="/vendas" element={<VendasPage />} />
-          <Route path="/clientes" element={<ClientesPage />} />
-          <Route path="/relatorios" element={<RelatoriosPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/" element={<Navigate to="/estoque" replace />} />
-        </Route>
-
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/estoque" replace />} />
+        <Route
+          path="/estoque"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <EstoquePage />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/vendas"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <VendasPage />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/clientes"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <ClientesPage />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/relatorios"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <RelatoriosPage />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <AdminPage />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
